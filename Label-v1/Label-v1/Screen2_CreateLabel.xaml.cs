43e8cde85cdefa813d58471f2f79dfc034e51ebd
Priyanshu-Bhtanagar <priyanshubhatnagar1851996@hotmail.com>
2016-09-13 19:39:53 +0530

    Pull everbody!

--- Label-v1/Label-v1/Screen2_CreateLabel.xaml.cs
+++ Label-v1/Label-v1/Screen2_CreateLabel.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -24,9 +25,9 @@ namespace Label_v1
     {
         //Variables
 
-        double varRows = 1, varColumn = 1;
-        int varAddButton = 0;
-
+        static double varRows = 1, varColumn = 1;
+        static int varAddButton = 0;
+        static string[] textArray = new string[8] { "Label Name", "Line 1", "Line 2", "Line 3", "Line 4", "Line 5", "Line 6" ,"Line 7"}; 
         //Main Function
         public Screen2_CreateLabel()
         {
@@ -83,6 +84,7 @@ namespace Label_v1
                 AddButton.IsEnabled = false;
                 Textbox7.Visibility = Visibility.Visible;
             }
+            funcCreateRows();
         }
 
         private void SubButtonClicked(object sender, RoutedEventArgs e)
@@ -126,6 +128,7 @@ namespace Label_v1
             {
                 SubButton.IsEnabled = false;
             }
+            funcCreateRows();
         }
 
         private void closePopup(object sender, RoutedEventArgs e)
@@ -139,7 +142,97 @@ namespace Label_v1
                 Frame.GoBack();
             }
         }
+        private void LabelNameTextBoxChanged(object sender, TextChangedEventArgs e)
+        {
+            textArray[0] = Textbox0.Text;
+            funcCreateRows();
+        }
+
+        private void Textbox1Changed(object sender, TextChangedEventArgs e)
+        {
+            textArray[1] = Textbox1.Text;
+            funcCreateRows();
+        }
+
+        private void Textbox2Changed(object sender, TextChangedEventArgs e)
+        {
+            textArray[2] = Textbox2.Text;
+            funcCreateRows();
+        }
+
+        private void Textbox3Changed(object sender, TextChangedEventArgs e)
+        {
+            textArray[3] = Textbox3.Text;
+            funcCreateRows();
+        }
+
+        private void Textbox4Changed(object sender, TextChangedEventArgs e)
+        {
+            textArray[4] = Textbox4.Text;
+            funcCreateRows();
+        }
+
+        private void Textbox5Changed(object sender, TextChangedEventArgs e)
+        {
+            textArray[5] = Textbox5.Text;
+            funcCreateRows();
+        }
+
+        private void Textbox6Changed(object sender, TextChangedEventArgs e)
+        {
+            textArray[6] = Textbox6.Text;
+            funcCreateRows();
+        }
+
+        private void Textbox7Changed(object sender, TextChangedEventArgs e)
+        {
+            textArray[7] = Textbox7.Text;
+            funcCreateRows();
+        }
+
+        private void MoreSettingsButton(object sender, RoutedEventArgs e)
+        {
+            if (!MoreSettingsPopup.IsOpen) { MoreSettingsPopup.IsOpen = true; }
+        }
 
         //Custom Functions
+
+        void funcClearRows() {
+            PreviewLabelGrid.Children.Clear();
+            PreviewLabelGrid.RowDefinitions.Clear();
+        }
+
+        void funcCreateRows() {
+            funcClearRows();
+            for (int i = 0; i < varAddButton; i++)
+            {
+                RowDefinition r = new RowDefinition();
+                PreviewLabelGrid.RowDefinitions.Add(r);
+            }
+            foreach(RowDefinition row in PreviewLabelGrid.RowDefinitions)
+            {
+                row.Height = new GridLength(1, GridUnitType.Star); row.Height = new GridLength(1, GridUnitType.Star);
+            }
+            for(int i = 0; i < PreviewLabelGrid.RowDefinitions.Count; i++)
+            {
+                var stack = new StackPanel();
+
+                stack.Orientation = Orientation.Vertical;
+
+                stack.Margin = new Thickness(0);
+                stack.SetValue(Grid.RowProperty, i);
+                stack.SetValue(Grid.ColumnProperty, 0);
+                PreviewLabelGrid.Children.Add(stack);
+
+                var textblck = new TextBlock();
+                textblck.HorizontalAlignment = HorizontalAlignment.Stretch;
+                textblck.VerticalAlignment = VerticalAlignment.Stretch;
+                
+                textblck.Text = textArray[i+1];
+                textblck.Foreground = new SolidColorBrush(Colors.Black);
+                stack.Children.Add(textblck);
+
+            }
+        }
     }
 }
