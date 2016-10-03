4ce49fdce29f4ee5ef94845a21b56efd31eb841b
Priyanshu-Bhtanagar <priyanshubhatnagar1851996@hotmail.com>
2016-10-04 01:16:34 +0530

    Beta Version

--- Label-v1/Label-v1/Label.cs
+++ Label-v1/Label-v1/Label.cs
@@ -4,21 +4,32 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace Label_v1
 {
     class Label
     {
         public int varAddButton = 0;
-        public double[] ArrRowColumn = new double[2] {1,1}; 
+        public double[] ArrRowColumn = new double[2] {1,1};
+        public string labelName; 
         public ObservableCollection<string> ArrText = new ObservableCollection<string>();
         public ObservableCollection<double> ArrTextFont = new ObservableCollection<double>();
         public ObservableCollection<double> ArrTextLeftMargin = new ObservableCollection<double>();
         public ObservableCollection<double> ArrTextTopMargin = new ObservableCollection<double>();
-        public ObservableCollection<double> ArrTextRightMargin = new ObservableCollection<double>();
-        public ObservableCollection<double> ArrTextBottomMargin = new ObservableCollection<double>();
-        public ObservableCollection<double> ArrTextHorizontalAlignMargin = new ObservableCollection<double>();
-        public ObservableCollection<double> ArrTextVerticalAlignMargin = new ObservableCollection<double>();
         public ObservableCollection<string> ArrTextFontFamily = new ObservableCollection<string>();
+
+        public int varImageCount = 0;
+        public ObservableCollection<BitmapImage> ImageBitmap = new ObservableCollection<BitmapImage>();
+        public ObservableCollection<double> ImageWidth = new ObservableCollection<double>();
+        public ObservableCollection<double> ImageHeight = new ObservableCollection<double>();
+        public ObservableCollection<string> ImageFileName = new ObservableCollection<string>();
+        public ObservableCollection<double> ImageHorizontalAlignment = new ObservableCollection<double>();
+        public ObservableCollection<double> ImageVerticalAlignment = new ObservableCollection<double>();
+        public ObservableCollection<double> ImageStretchAlignment = new ObservableCollection<double>();
+        public ObservableCollection<double> ImageLeftMargin = new ObservableCollection<double>();
+        public ObservableCollection<double> ImageTopMargin = new ObservableCollection<double>();
+        public ObservableCollection<double> ImageRightMargin = new ObservableCollection<double>();
+        public ObservableCollection<double> ImageBottomMargin = new ObservableCollection<double>();
     }
 }
--- Label-v1/Label-v1/Screen2_CreateLabel.xaml.cs
+++ Label-v1/Label-v1/Screen2_CreateLabel.xaml.cs
@@ -5,13 +5,17 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Streams;
 using Windows.UI;
+using Windows.UI.Input;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -26,8 +30,10 @@ namespace Label_v1
         //Variables
         Label label = new Label();
         int selectedIndex;
-        bool validState = false;
+        bool validState = false, imageValid=false, textMovedValid=false;
         string[] allFonts = Microsoft.Graphics.Canvas.Text.CanvasTextFormat.GetSystemFontFamilies();
+        UIElement movableElement;
+        PointerPoint ptr;
         //Main Function
         public Screen2_CreateLabel()
         {
@@ -76,7 +82,6 @@ namespace Label_v1
                 ++label.varAddButton;
                 if (label.varAddButton > 0)
                 {
-                    MoreSettingsButton.IsEnabled = true;
                     SubButton.IsEnabled = true;
                 }
                  funcCreateLabel();
@@ -96,7 +101,6 @@ namespace Label_v1
                 label.varAddButton--;
                 if (label.varAddButton == 0)
                 {
-                    MoreSettingsButton.IsEnabled = false;
                     SubButton.IsEnabled = false;
                 }
                  funcCreateLabel();
@@ -105,11 +109,6 @@ namespace Label_v1
             {
 
             }
-        }
-
-        private void closePopup(object sender, RoutedEventArgs e)
-        {
-
         }
 
         private void BackButtonClicked(object sender, RoutedEventArgs e)
@@ -124,8 +123,7 @@ namespace Label_v1
             if (!validState) return;
             try
             {
-                label.ArrText[0] = Textbox0.Text;
-                funcCreateLabel();
+                label.labelName = Textbox0.Text;
             }
             catch
             {
@@ -134,129 +132,29 @@ namespace Label_v1
         }
 
 
-        private void MoreSettingsButtonClicked(object sender, RoutedEventArgs e)
-        {
-            if (!validState) return;
-            CreateLabelSettingsScrollViewer.Visibility = Visibility.Collapsed;
-            MoreSettingsScrollView.Visibility = Visibility.Visible;
-
-
-            foreach (StackPanel x in PreviewLabelGrid.Children)
-            {
-                x.BorderThickness = new Thickness(1);
-                x.BorderBrush = new SolidColorBrush(Colors.DarkGray);
-            }
-        }
-
-        private void MoveLeftSliderChanged(object sender, RangeBaseValueChangedEventArgs e)
-        {
-            if (!validState) return;
-            try
-            {
-                label.ArrTextRightMargin[selectedIndex] = MoveLeftSlider.Value;
-                funcCreateLabel();
-            }
-            catch
-            {
-
-            }
-        }
-
-        private void MoveBottomSliderChanged(object sender, RangeBaseValueChangedEventArgs e)
-        {
-            if (!validState) return;
-            try
-            {
-                label.ArrTextTopMargin[selectedIndex] = MoveBottomSlider.Value;
-                funcCreateLabel();
-            }
-            catch
-            {
-
-            }
-        }
-
-        private void MoveUpSliderChanged(object sender, RangeBaseValueChangedEventArgs e)
-        {
-            if (!validState) return;
-            try
-            {
-                label.ArrTextBottomMargin[selectedIndex] = MoveUpSlider.Value;
-                funcCreateLabel();
-            }
-            catch
-            {
-
-            }
-        }
-
-        private void MoveRightSLiderChanged(object sender, RangeBaseValueChangedEventArgs e)
-        {
-            if (!validState) return;
-            try
-            {
-                label.ArrTextLeftMargin[selectedIndex] = MoveRightSlider.Value;
-                funcCreateLabel();
-            }
-            catch
-            {
-
-            }
-        }
+   //TextBlock functions
 
         private void SliderFontSizeChanged(object sender, RangeBaseValueChangedEventArgs e)
         {
             if (!validState) return;
             try
             {
+                (PreviewLabelGrid.Children.ElementAt(selectedIndex) as TextBlock).FontSize = SliderFontSize.Value;
                 label.ArrTextFont[selectedIndex] = SliderFontSize.Value;
-                funcCreateLabel();
             }
             catch
             {
 
             }
         }
-        private void XAlignmentChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (!validState) return;
-            try
-            {
-                if ((XAlignment.SelectedItem as ComboBoxItem).Content.ToString() == "Center") label.ArrTextHorizontalAlignMargin[selectedIndex] = 0;
-                else if ((XAlignment.SelectedItem as ComboBoxItem).Content.ToString() == "Left") label.ArrTextHorizontalAlignMargin[selectedIndex] = 1;
-                else if ((XAlignment.SelectedItem as ComboBoxItem).Content.ToString() == "Right") label.ArrTextHorizontalAlignMargin[selectedIndex] = 2;
-                funcCreateLabel();
-            }
-            catch
-            {
-
-            }
-        }
-
-        private void YAlignmentChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (!validState) return;
-            try
-            {
-                if ((YAlignment.SelectedItem as ComboBoxItem).Content.ToString() == "Center") label.ArrTextVerticalAlignMargin[selectedIndex] = 0;
-                else if ((YAlignment.SelectedItem as ComboBoxItem).Content.ToString() == "Top") label.ArrTextVerticalAlignMargin[selectedIndex] = 1;
-                else if ((YAlignment.SelectedItem as ComboBoxItem).Content.ToString() == "Bottom") label.ArrTextVerticalAlignMargin[selectedIndex] = 2;
-                funcCreateLabel();
-            }
-            catch
-            {
-
-            }
-        }
-
 
         private void ComboboxFontsChanged(object sender, SelectionChangedEventArgs e)
         {
             if (!validState) return;
             try
             {
+                (PreviewLabelGrid.Children.ElementAt(selectedIndex) as TextBlock).FontFamily = new FontFamily(ComboboxFonts.SelectedItem.ToString());
                 label.ArrTextFontFamily[selectedIndex] = ComboboxFonts.SelectedItem.ToString();
-                funcCreateLabel();
             }
             catch
             {
@@ -271,19 +169,7 @@ namespace Label_v1
             if (!validState) return;
             CreateLabelSettingsScrollViewer.Visibility = Visibility.Visible;
             MoreSettingsScrollView.Visibility = Visibility.Collapsed;
-
-
-            foreach (StackPanel x in PreviewLabelGrid.Children)
-            {
-                x.BorderThickness = new Thickness(0);
-                x.BorderBrush = new SolidColorBrush(Colors.Transparent);
-            }
-            XAlignment.IsEnabled = false;
-            YAlignment.IsEnabled = false;
-            MoveUpSlider.IsEnabled = false;
-            MoveLeftSlider.IsEnabled = false;
-            MoveRightSlider.IsEnabled = false;
-            MoveBottomSlider.IsEnabled = false;
+           
             SliderFontSize.IsEnabled = false;
             ComboboxFonts.IsEnabled = false;
 
@@ -323,12 +209,8 @@ namespace Label_v1
             button.Width = 32;
             button.Height = 32;
 
-                label.ArrTextHorizontalAlignMargin.Insert(label.varAddButton, 0);
-                label.ArrTextVerticalAlignMargin.Insert(label.varAddButton, 0);
                 label.ArrTextLeftMargin.Insert(label.varAddButton,0);
-                label.ArrTextRightMargin.Insert(label.varAddButton, 0);
                 label.ArrTextTopMargin.Insert(label.varAddButton, 0);
-                label.ArrTextBottomMargin.Insert(label.varAddButton, 0);
                 label.ArrTextFont.Insert(label.varAddButton, 15);
                 label.ArrTextFontFamily.Insert(label.varAddButton,"Arial");
                 label.ArrText.Insert(label.varAddButton, "Text Line " + (label.varAddButton + 1));
@@ -347,12 +229,8 @@ namespace Label_v1
             StackTextBox.Children.RemoveAt(num);
 
             label.ArrText.RemoveAt(num);
-            label.ArrTextHorizontalAlignMargin.RemoveAt(num);
-            label.ArrTextVerticalAlignMargin.RemoveAt(num);
             label.ArrTextLeftMargin.RemoveAt(num);
-            label.ArrTextRightMargin.RemoveAt(num);
             label.ArrTextTopMargin.RemoveAt(num);
-            label.ArrTextBottomMargin.RemoveAt(num);
             label.ArrTextFontFamily.RemoveAt(num);
             label.ArrTextFont.RemoveAt(num);
 
@@ -401,9 +279,6 @@ namespace Label_v1
             funcClearLabel();
             for(int i=0; i<label.varAddButton; i++)
             {
-                var stack = new StackPanel();
-                stack.Margin = new Thickness(label.ArrTextLeftMargin[i], label.ArrTextTopMargin[i], label.ArrTextRightMargin[i], label.ArrTextBottomMargin[i]);
-
                 var textblock = new TextBlock();
                 textblock.Name = i.ToString();
                 textblock.FontSize = label.ArrTextFont[i];
@@ -411,29 +286,52 @@ namespace Label_v1
                 textblock.Foreground = new SolidColorBrush(Colors.Black);
                 textblock.Text = label.ArrText[i];
                 textblock.Tapped += Textblock_Tapped;
-
-                if (label.ArrTextHorizontalAlignMargin[i] == 0) stack.HorizontalAlignment = HorizontalAlignment.Center;
-                else if (label.ArrTextHorizontalAlignMargin[i] == 1) stack.HorizontalAlignment = HorizontalAlignment.Left;
-                else if (label.ArrTextHorizontalAlignMargin[i] == 2) stack.HorizontalAlignment = HorizontalAlignment.Right;
-
-                if (label.ArrTextVerticalAlignMargin[i] == 0) stack.VerticalAlignment = VerticalAlignment.Center;
-                else if (label.ArrTextVerticalAlignMargin[i] == 1) stack.VerticalAlignment = VerticalAlignment.Top;
-                else if (label.ArrTextVerticalAlignMargin[i] == 2) stack.VerticalAlignment = VerticalAlignment.Bottom;
-
-                stack.Children.Add(textblock);
-
-                PreviewLabelGrid.Children.Add(stack);
+                textblock.PointerPressed += Textblock_PointerPressed;
+                textblock.Margin = new Thickness(label.ArrTextLeftMargin[i], label.ArrTextTopMargin[i],0,0);
+                textblock.HorizontalAlignment = HorizontalAlignment.Left;
+                textblock.VerticalAlignment = VerticalAlignment.Top;
                 
-            }
-            
-        }
+                PreviewLabelGrid.Children.Add(textblock);
 
+            }
+            if (!imageValid) return;
+
+            for(int i = 0; i<label.varImageCount; i++)
+            {
+                var image = new Image();
+                image.Name = label.ImageFileName[i];
+                image.Source = label.ImageBitmap[i];
+                image.Width = label.ImageWidth[i];
+                image.Height = label.ImageHeight[i];
+                image.Margin = new Thickness(label.ImageLeftMargin[i], label.ImageTopMargin[i], label.ImageRightMargin[i], label.ImageBottomMargin[i]);
+                image.Tapped += Image_Tapped;
+                if (label.ImageHorizontalAlignment[i] == 0) image.HorizontalAlignment = HorizontalAlignment.Center;
+                else if (label.ImageHorizontalAlignment[i] == 1) image.HorizontalAlignment = HorizontalAlignment.Left;
+                else if (label.ImageHorizontalAlignment[i] == 2) image.HorizontalAlignment = HorizontalAlignment.Right;
+
+                if (label.ImageVerticalAlignment[i] == 0) image.VerticalAlignment = VerticalAlignment.Center;
+                else if (label.ImageVerticalAlignment[i] == 1) image.VerticalAlignment = VerticalAlignment.Top;
+                else if (label.ImageVerticalAlignment[i] == 2) image.VerticalAlignment = VerticalAlignment.Bottom;
+
+                if (label.ImageStretchAlignment[i] == 0) image.Stretch = Stretch.None;
+                else if (label.ImageStretchAlignment[i] == 1) image.Stretch = Stretch.Fill;
+                else if (label.ImageStretchAlignment[i] == 2) image.Stretch = Stretch.Uniform;
+                else if (label.ImageStretchAlignment[i] == 3) image.Stretch = Stretch.UniformToFill;
+                
+                PreviewLabelGrid.Children.Add(image);
+            }
+
+
+        }
         private void Textblock_Tapped(object sender, TappedRoutedEventArgs e)
         {
             if (!validState) return;
             try
             {
                 validState = false;
+
+                CreateLabelSettingsScrollViewer.Visibility = Visibility.Collapsed;
+                MoreSettingsScrollView.Visibility = Visibility.Visible;
 
                 var textblock = sender as TextBlock;
                 selectedIndex = Convert.ToInt32(textblock.Name);
@@ -442,38 +340,121 @@ namespace Label_v1
                 StackStatusTextBlock.BorderThickness = new Thickness(2);
                 StackStatusTextBlock.BorderBrush = new SolidColorBrush(Colors.Wheat);
 
-                XAlignment.IsEnabled = true;
-                YAlignment.IsEnabled = true;
-                MoveUpSlider.IsEnabled = true;
-                MoveLeftSlider.IsEnabled = true;
-                MoveRightSlider.IsEnabled = true;
-                MoveBottomSlider.IsEnabled = true;
                 SliderFontSize.IsEnabled = true;
                 ComboboxFonts.IsEnabled = true;
 
                 ComboboxFonts.SelectedIndex = ComboboxFonts.Items.IndexOf(label.ArrTextFontFamily[selectedIndex]);
-
-                if (label.ArrTextHorizontalAlignMargin[selectedIndex] == 0) XAlignment.SelectedIndex = 0;
-                else if (label.ArrTextHorizontalAlignMargin[selectedIndex] == 1) XAlignment.SelectedIndex = 1;
-                else if (label.ArrTextHorizontalAlignMargin[selectedIndex] == 2) XAlignment.SelectedIndex = 2;
-
-                if (label.ArrTextVerticalAlignMargin[selectedIndex] == 0) YAlignment.SelectedIndex = 0;
-                else if (label.ArrTextVerticalAlignMargin[selectedIndex] == 1) YAlignment.SelectedIndex = 1;
-                else if (label.ArrTextVerticalAlignMargin[selectedIndex] == 2) YAlignment.SelectedIndex = 2;
-
-                MoveUpSlider.Value = label.ArrTextBottomMargin[selectedIndex];
-                MoveLeftSlider.Value = label.ArrTextRightMargin[selectedIndex];
-                MoveRightSlider.Value = label.ArrTextLeftMargin[selectedIndex];
-                MoveBottomSlider.Value = label.ArrTextTopMargin[selectedIndex];
-
                 SliderFontSize.Value = label.ArrTextFont[selectedIndex];
+
                 validState = true;
             }
             catch
             {
 
             }
+
+        }
+
+        //TextBlock Pointer
+        private void Textblock_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            movableElement = sender as UIElement;
+            ptr = e.GetCurrentPoint(movableElement);
+            textMovedValid = true;
+        }
+
+        private void TextBlock_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            e.Handled = true;
+            textMovedValid = false;
+        }
+        private void Textblock_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            e.Handled = true;
+            textMovedValid = false;
+            ptr = null;
+        }
+
+        private void Textblock_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            if (!textMovedValid) return;
+            try
+            {
+                PointerPoint ptrtemp = e.GetCurrentPoint(movableElement);
+                if (!ptrtemp.IsInContact) return;
+
+                var textblock = movableElement as TextBlock;
+                double marginX = ptrtemp.Position.X - ptr.Position.X + textblock.Margin.Left;
+                double marginY = ptrtemp.Position.Y - ptr.Position.Y + textblock.Margin.Top;
+                e.Handled = true;
+                        if (marginX < 0 && marginY < 0) textblock.Margin = new Thickness(textblock.Margin.Left, textblock.Margin.Top, 0, 0);
+                        else if (marginX < 0 && marginY >= 0) textblock.Margin = new Thickness(textblock.Margin.Left, marginY, 0, 0);
+                        else if (marginX >= 0 && marginY < 0) textblock.Margin = new Thickness(marginX, textblock.Margin.Top, 0, 0);
+                        else if (marginX >= 0 && marginY >= 0) textblock.Margin = new Thickness(marginX, marginY, 0, 0);
+                        label.ArrTextLeftMargin[Convert.ToInt32(textblock.Name)] = textblock.Margin.Left;
+                        label.ArrTextTopMargin[Convert.ToInt32(textblock.Name)] = textblock.Margin.Top;
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Image_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+           /* MoreSettingsScrollView.Visibility = Visibility.Collapsed;
+            ImageSettings.Visibility = Visibility.Visible;
+            */
+        }   
+
+        private async void ImageButtonClicked(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string FileName = Textbox0.Text.ToString();
+                StorageFolder _Folder = ApplicationData.Current.LocalFolder;
+
+                var picker = new Windows.Storage.Pickers.FileOpenPicker();
+                picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+                picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+                picker.FileTypeFilter.Add(".jpg");
+                picker.FileTypeFilter.Add(".jpeg");
+                picker.FileTypeFilter.Add(".png");
+
+                StorageFile file = await picker.PickSingleFileAsync();
+
+                if (file != null)
+                {
+                    BitmapImage bitmapImage = new BitmapImage();
+                    FileRandomAccessStream stream = (FileRandomAccessStream)await file.OpenAsync(FileAccessMode.Read);
+                    bitmapImage.SetSource(stream);
+
+                    ++label.varImageCount;
+
+                    label.ImageFileName.Add("Image_" + label.varImageCount);
+                    label.ImageBitmap.Add(bitmapImage);
+                    label.ImageWidth.Add(70);
+                    label.ImageHeight.Add(70);
+                    label.ImageHorizontalAlignment.Add(0);
+                    label.ImageVerticalAlignment.Add(0);
+                    label.ImageStretchAlignment.Add(2);
+                    label.ImageLeftMargin.Add(0);
+                    label.ImageTopMargin.Add(0);
+                    label.ImageRightMargin.Add(0);
+                    label.ImageBottomMargin.Add(0);
+
+                    imageValid = true;
+                    funcCreateLabel();
+                }
+
+            }catch
+                {
+
+                }
             
+            }
+
+        
         }
     }
-}
